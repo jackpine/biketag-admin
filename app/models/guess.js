87ad19b724d0dc1b0b367ad95adfba1629006f6a
Michael Kirk <michael.john.kirk@gmail.com>
2015-03-31 14:09:18 -0700

    show spot image,location and guess location

--- app/models/guess.js
+++ app/models/guess.js
@@ -3,5 +3,20 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   spot: DS.belongsTo('spot', { async: true }),
   createdAt: DS.attr('date'),
-  location: DS.attr()
+  location: DS.attr(),
+  longitude: function() {
+    if( this.get('location') === undefined ) {
+      return null;
+    } else {
+      return this.get('location')['coordinates'][0];
+    }
+  }.property('location'),
+
+  latitude: function() {
+    if( this.get('location') === undefined ) {
+      return null;
+    } else {
+      return this.get('location')['coordinates'][1];
+    }
+  }.property('location')
 });
--- app/models/spot.js
+++ app/models/spot.js
@@ -2,6 +2,25 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   game: DS.belongsTo('game'),
+  guesses: DS.hasMany('guess', { async: true }),
+
+  imageUrl: DS.attr(),
   location: DS.attr(),
-  createdAt: DS.attr('date')
+  createdAt: DS.attr('date'),
+
+  longitude: function() {
+    if( this.get('location') === undefined ) {
+      return null;
+    } else {
+      return this.get('location')['coordinates'][0];
+    }
+  }.property('location'),
+
+  latitude: function() {
+    if( this.get('location') === undefined ) {
+      return null;
+    } else {
+      return this.get('location')['coordinates'][1];
+    }
+  }.property('location')
 });
