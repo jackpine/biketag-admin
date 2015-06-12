3e40d4d1ee5d16dabe9c5b24f26bbbec4337af48
Michael Kirk <michael.john.kirk+michael.john.kirk@gmail.com>
2015-06-12 13:37:15 -0700

    add foundation/normalize css

--- Brocfile.js
+++ Brocfile.js
@@ -17,6 +17,8 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
+app.import('bower_components/foundation/css/normalize.css');
+app.import('bower_components/foundation/css/foundation.css');
 app.import('bower_components/moment/moment.js');
 
 module.exports = app.toTree();
