d458783b680803c0d9ee02cb76ef88ea8ad02a33
Michael Kirk <michael.john.kirk@gmail.com>
2015-08-13 11:01:15 -0700

    store secrets url in secrets

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 import config from '../config/environment';
 
 export default DS.RESTAdapter.extend({
-  host: 'http://api.biketag.jackpine.me',
+  host: config.secrets.host,
   namespace: 'api/v1',
   headers: {
     Authorization: 'Token token="' + config.secrets.apiKey + '"'
