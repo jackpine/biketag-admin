2b5d2265387cdac557515a9901f6f06728b658c3
Michael Kirk <michael@jackpine.me>
2015-08-31 16:35:07 -0700

    plot markers

--- app/components/map-plot.js
+++ app/components/map-plot.js
@@ -5,56 +5,31 @@ export default Ember.Component.extend({
     attributeBindings: ['id'],
     id: 'map',
 
-//    // this is what doesn't work
-//    markersObserver: function() {
-//        // retrieve the map - ??
-//        // retrieve the markerLayer on the map - ??
-//        var markerLayer = // ...
-//
-//            // regenerate the geoJSON with the new marker positions
-//            var geoJSON = generateGeoJSON(markers);
-//
-//        // re-set the markerLayer's geoJSON
-//        markerLayer.setGeoJSON(geoJSON);
-//    }.observes('markers'),
-
-    // works! adds the map and markers as expected
     didInsertElement: function() {
+        console.log("location", this.get('location'));
+        this.get('location') || this.set('location', [35, -118]);
+
         // create the map
         L.mapbox.accessToken = config.secrets.mapboxAccessToken;
-        var map = L.mapbox.map('map', 'mapbox.streets');
+        var map = L.mapbox.map('map', 'mapbox.streets').
+            setView(this.get('location'), 6)
 
         // add a featureLayer to hold all the markers
         var markerLayer = L.mapbox.featureLayer().addTo(map);
 
         var markers = this.get('markers');
-        var features = [];
+        var features = _.map(markers, function(marker) {
+            return {
+                type: 'Feature',
+                geometry: marker.get("location")
+            }
+        });
 
-//        markers.map(function(marker) {
-//            var latitude = marker.get('vehicle_lat');
-//            var longitude = marker.get('vehicle_lon');
-//
-//            features.push({
-//                type: 'Feature',
-//                geometry: {
-//                    type: 'Point',
-//                    coordinates: [longitude, latitude]
-//                },
-//                properties: {
-//                    'marker-size': 'large',
-//                    'marker-symbol': 'rail-light',
-//                    'marker-color': '#f86767'
-//                }
-//            });
-        },
-
-//        var geoJSON = {
-//            type: 'FeatureCollection',
-//            features: features
-//        };
-//
-//        // set the markers as the GeoJSON on the markerLayer
-//        markerLayer.setGeoJSON(geoJSON);
-//    }
+        var geoJSON = {
+            type: 'FeatureCollection',
+            features: features
+        };
+        markerLayer.setGeoJSON(geoJSON);
+    }
 
 });
