3495270eb43dddf8f1d7acab66c9d7e65c15aab1
Michael Kirk <michael@jackpine.me>
2015-08-31 17:59:54 -0700

    kludge to get maplinks to work for spots and guesses

--- app/components/map-plot.js
+++ app/components/map-plot.js
@@ -2,34 +2,29 @@ import Ember from 'ember';
 import config from '../config/environment';
 
 export default Ember.Component.extend({
-    attributeBindings: ['id'],
-    id: 'map',
-
     didInsertElement: function() {
-        console.log("location", this.get('location'));
-        this.get('location') || this.set('location', [35, -118]);
+        var location = [34.1, -118.4];
 
         // create the map
         L.mapbox.accessToken = config.secrets.mapboxAccessToken;
-        var map = L.mapbox.map('map', 'mapbox.streets').
-            setView(this.get('location'), 6)
+        var map = L.mapbox.map('map', 'mapbox.streets')
+            .setView(location, 11);
 
-        // add a featureLayer to hold all the markers
-        var markerLayer = L.mapbox.featureLayer().addTo(map);
+        // we'll put all the markers in one layer
+        var markerLayer = L.mapbox.featureLayer();
 
-        var markers = this.get('markers');
-        var features = _.map(markers, function(marker) {
-            return {
-                type: 'Feature',
-                geometry: marker.get("location")
-            }
+        var features = this.get('features');
+        var component = this;
+        var markers = _.map(features, function(feature) {
+            var url = '/' + component.get('urlPrefix') + '/' + feature.get('id');
+            return L.marker([feature.get('latitude'), feature.get('longitude')])
+                .bindPopup('<a href="' + url + '">'+ feature.get('id') +'</a>');
         });
 
-        var geoJSON = {
-            type: 'FeatureCollection',
-            features: features
-        };
-        markerLayer.setGeoJSON(geoJSON);
+        _.each(markers, function(marker) {
+            marker.addTo(markerLayer);
+        });
+        markerLayer.addTo(map);
     }
 
 });
