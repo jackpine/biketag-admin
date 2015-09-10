51208bf2d67812a000163713524dddfc39c46c84
Michael Kirk <michael.john.kirk@gmail.com>
2015-09-10 11:50:51 -0700

    include author name in spots/guesses tempaltes

--- app/models/guess.js
+++ app/models/guess.js
@@ -5,6 +5,13 @@ export default DS.Model.extend({
   game: DS.belongsTo('game', { async: true }),
   createdAt: DS.attr('date'),
   imageUrl: DS.attr(),
+  userName: DS.attr(),
+  correct: DS.attr(),
+  incorrect: function() {
+    //FIXME this is because I don't know how to do an else
+    //in the handlebar template and have no wifi to look it up.
+    return !this.get('correct');
+  }.property('correct'),
   order: function() {
     return parseInt(this.get('id'), 10);
   }.property('id'),
