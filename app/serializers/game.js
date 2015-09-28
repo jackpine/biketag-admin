77065d4c8fb39a79dba14acb532844db96739854
Michael Kirk <michael.john.kirk@gmail.com>
2015-09-27 17:14:29 -0700

    use separate activemodel adapter, rather than deprecated included one

--- app/serializers/game.js
+++ app/serializers/game.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
+import { ActiveModelSerializer } from 'active-model-adapter';
 
-export default DS.ActiveModelSerializer.extend(DS.EmbeddedRecordsMixin, {
+export default ActiveModelSerializer.extend(DS.EmbeddedRecordsMixin, {
   attrs: {
     current_spot: { embedded: 'always' }
   }
