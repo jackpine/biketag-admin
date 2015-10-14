a7b2ef069bec8d6285f230dc89d028d888190a6b
Michael Kirk <michael@jackpine.me>
2015-10-14 13:14:42 -0700

    orphan commit. i think it was centering a map on the guess

--- app/components/map-plot.js
+++ app/components/map-plot.js
@@ -3,27 +3,41 @@ import config from '../config/environment';
 
 export default Ember.Component.extend({
     didInsertElement: function() {
-        var location = [34.1, -118.4];
+        var defaultLocation = [34.1, -118.4];
 
         // create the map
         L.mapbox.accessToken = config.secrets.mapboxAccessToken;
         var map = L.mapbox.map('map', 'mapbox.streets')
-            .setView(location, 11);
+            .setView(defaultLocation, 11);
 
         // we'll put all the markers in one layer
         var markerLayer = L.mapbox.featureLayer();
 
-        var features = this.get('features');
-        var component = this;
-        var markers = _.map(features, function(feature) {
-            var url = '/' + component.get('urlPrefix') + '/' + feature.get('id');
-            return L.marker([feature.get('latitude'), feature.get('longitude')])
-                .bindPopup('<a href="' + url + '">'+ feature.get('id') +'</a>');
-        });
+        var focus = this.get('focus');
+        console.log('focus', focus);
+        if (focus) {
+            var focusLocation = [focus.get('latitude'), focus.get('longitude')];
+            var marker = L.marker(focusLocation);
+            marker.addTo(map);
 
-        _.each(markers, function(marker) {
-            marker.addTo(markerLayer);
-        });
+            map.setView(focusLocation);
+        }
+
+        var features = this.get('features');
+        console.log('features', features);
+        if (features) {
+            var component = this;
+            var markers = _.map(features, function(feature) {
+                var url = '/' + component.get('urlPrefix') + '/' + feature.get('id');
+                return L.marker([feature.get('latitude'), feature.get('longitude')])
+                    .bindPopup('<a href="' + url + '">'+ feature.get('id') +'</a>');
+            });
+
+            _.each(markers, function(marker) {
+                marker.addTo(markerLayer);
+            });
+        }
+
         markerLayer.addTo(map);
     }
 
