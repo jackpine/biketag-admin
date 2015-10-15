ae827b5f6c41e88993140426150951acac18bcde
Michael Kirk <michael@jackpine.me>
2015-10-14 18:32:51 -0700

    game map of current spots

--- app/models/game.js
+++ app/models/game.js
@@ -7,4 +7,36 @@ export default DS.Model.extend({
   order: function() {
     return parseInt(this.get('id'), 10);
   }.property('id'),
+
+  location: function() {
+    return this.get('currentSpot').get('location');
+  }.property('currentSpot'),
+
+  longitude: function() {
+    if( this.get('location') === undefined ) {
+      return null;
+    } else {
+      return this.get('location')['coordinates'][0];
+    }
+  }.property('location'),
+
+  latitude: function() {
+    if( this.get('location') === undefined ) {
+      return null;
+    } else {
+      return this.get('location')['coordinates'][1];
+    }
+  }.property('location'),
+
+  formattedLocation: function() {
+    var latitude = this.get('latitude');
+    var longitude = this.get('longitude');
+    if( latitude === undefined || longitude === undefined) {
+      return "missing";
+    } else {
+      var latLabel = (latitude > 0) ? "N" : "S";
+      var lonLabel = (longitude > 0) ? "E" : "W";
+      return Math.abs(latitude) + "°" + latLabel + " " + Math.abs(longitude) + "°" + lonLabel;
+    }
+  }.property('latitude', 'longitude')
 });
