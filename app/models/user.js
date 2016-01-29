741520d7f9ca655a1ffdcdec4ce89136386234d1
Michael Kirk <michael.john.kirk@gmail.com>
2016-01-28 20:51:48 -0800

    order users

--- app/models/user.js
+++ app/models/user.js
@@ -6,6 +6,9 @@ export default DS.Model.extend({
   spots: DS.hasMany('spot', { inverse: "user" }),
   guesses: DS.hasMany('guess', { inverse: "user" }),
   createdAt: DS.attr('date'),
+  order: function() {
+    return 0 - parseInt(this.get('id'), 10);
+  }.property('id'),
   formattedCreatedAt: function() {
     return moment(this.get('createdAt')).fromNow();
   }.property('createdAt')
